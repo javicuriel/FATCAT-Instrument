0b12d854072908c09ecf626b9aea05dfffe57c05
javicuriel <javiercuriel94@gmail.com>
2018-05-04 16:02:33 +0200

    Added working example of object inheritance for messages

--- test.py
+++ test.py
@@ -1,20 +1,62 @@
-from Models import *
-import datetime
-import numpy as np
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import with_polymorphic
 
-ppmtoug = 12.01/22.4
-t1 = Message.select().where(Message.countdown == 70).order_by(Message.timestamp.desc()).limit(1).get().timestamp
-t0 = t1 - datetime.timedelta(seconds = 5)
-t2 = t1 + datetime.timedelta(seconds = 630)
-baseline = Message.select(pw.fn.AVG(Message.co2).alias('avg')).where(Message.timestamp >= t0 and Message.timestamp <= t1).get().avg
-messages = Message.select(Message.co2, Message.flow, Message.runtime).where(Message.timestamp >= t1 and Message.timestamp <= t2)
-flowrate = messages.select(pw.fn.AVG(Message.flow).alias('avg')).get().avg
-co2 = []
-runtime = []
+Base = declarative_base()
+
+class Message(Base):
+    __tablename__ = 'message'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(50))
+    timestamp = Column(String(250))
+    sent = Column(Boolean)
+    topic = Column(String(250))
+    __mapper_args__ = {
+        'polymorphic_identity':'message',
+        'polymorphic_on':type
+    }
+    def to_json(self):
+        omit = {'id','sent','topic','_sa_instance_state'}
+        data = {x: self.__dict__[x] for x in self.__dict__ if x not in omit}
+        print(data)
+
+
+class Analysis(Message):
+    __tablename__ = 'analysis'
+    id = Column(Integer, ForeignKey('message.id') ,primary_key=True)
+    cosa_nueva = Column(String(250))
+    __mapper_args__ = {
+        'polymorphic_identity':'analysis',
+    }
+
+
+class Sample(Message):
+    __tablename__ = 'sample'
+    id = Column(Integer, ForeignKey('message.id') ,primary_key=True)
+    cosa_nueva_otra = Column(String(250))
+    __mapper_args__ = {
+        'polymorphic_identity':'sample'
+    }
+engine = create_engine('sqlite:///sqlalchemy_example.db')
+Base.metadata.create_all(engine)
+
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+new_analisis = Analysis(cosa_nueva='analisis', timestamp="1234")
+new_sample = Sample(cosa_nueva_otra='sammple', timestamp="1234")
+session.add(new_analisis)
+session.add(new_sample)
+session.commit()
+
+eng_plus_manager = with_polymorphic(Message, [Analysis, Sample])
+
+messages = session.query(eng_plus_manager).all()
 for m in messages:
-    co2.append((m.co2 - baseline)*ppmtoug)
-    runtime.append(m.runtime)
-
-deltatc = np.array(co2)*flowrate
-total_carbon = np.trapz(deltatc, x=np.array(runtime))
-print(total_carbon)
+    m.to_json()
