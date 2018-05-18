4f5311fa8a7652516da5b708ce3de871704928de
javicuriel <javiercuriel94@gmail.com>
2018-05-18 12:19:50 +0200

    Updated installer for serial

--- installer.py
+++ installer.py
@@ -1,6 +1,9 @@
 import os
 import requests
 import getpass
+import serial
+import re
+# import serial.tools.list_ports
 
 
 config_file = "/etc/systemd/system/instrument.service"
@@ -11,6 +14,36 @@ def create_script(uuid, auth_token):
     setup = "[Unit]\nDescription=Carbon measurement system\nAfter=network.target\n\n[Service]\nExecStart=/usr/bin/python -u main.py\nWorkingDirectory=/GAW-Instrument/\nEnvironment=MQTT_UUID=%s\nEnvironment=IBM_TOKEN=%s\nStandardOutput=inherit\nStandardError=inherit\nRestart=always\nRestartSec=2\n\n[Install]\nWantedBy=sysinit.target"
     file = open(config_file, "w+")
     file.write(setup % (uuid, auth_token))
+
+def lookup_port():
+    # Looks for and returns port with description
+    ports = serial.tools.list_ports.comports()
+    for port in ports:
+        return port if 'nano-TD' in port else None
+
+
+def set_up_serial(self):
+    # Waits 2 seconds before trying again if no port found
+    # and doubles time each try with maximum 32 second waiting time
+    port = lookup_port()
+    wait = 2
+    while not port:
+        print("No TCA found, waiting " + str(wait) + " seconds...")
+        time.sleep(wait)
+        if wait < 32:
+            wait = wait*2
+        port = lookup_port()
+
+    print("Serial port found: " + port.device)
+
+    return serial.Serial(
+        port = port.device,
+        baudrate = self.serial_baudrate,
+        parity = self.serial_parity,
+        stopbits = self.serial_stopbits,
+        bytesize = self.serial_bytesize,
+        timeout = self.serial_timeout
+    )
 
 def main():
     if os.path.exists(config_file):
@@ -31,9 +64,16 @@ def main():
         try:
             val = int(answer)
             if(val == 1):
-                # TODO
-                # Stop serial data flow and ask for id
-                uuid = raw_input("Enter UUID: ")
+                # Get Serial
+                nano_td_serial = set_up_serial()
+                # Stop data flow
+                nano_td_serial.write("X0000")
+                # Ask for serial number
+                nano_td_serial.write("N?")
+                # Get serial number
+                serial_number_response = nano_td_serial.readline().rstrip('\n')
+                # Regex for SN
+                uuid = re.match('Serial Number=(.*)', serial_number_response).group(1)
                 location = raw_input("Enter location: ")
                 lat = raw_input("Enter latitude: ")
                 long = raw_input("Enter longitude: ")
