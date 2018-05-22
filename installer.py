a509a3a4ff2569f61f7506bcc8934c4e3c9c86e8
javicuriel <javiercuriel94@gmail.com>
2018-05-22 15:51:08 +0200

    Get from config file

--- installer.py
+++ installer.py
@@ -2,47 +2,51 @@ import os
 import requests
 import getpass
 import serial
+import serial.tools.list_ports
 import re
-# import serial.tools.list_ports
+import configparser
+import time
 
 
 config_file = "/etc/systemd/system/instrument.service"
 url = "https://carbonmeasurmentsystem.eu-gb.mybluemix.net/"
-# config_file = "./instrument.service"
 
 def create_script(uuid, auth_token):
     setup = "[Unit]\nDescription=Carbon measurement system\nAfter=network.target\n\n[Service]\nExecStart=/usr/bin/python -u main.py\nWorkingDirectory=/GAW-Instrument/\nEnvironment=MQTT_UUID=%s\nEnvironment=IBM_TOKEN=%s\nStandardOutput=inherit\nStandardError=inherit\nRestart=always\nRestartSec=2\n\n[Install]\nWantedBy=sysinit.target"
     file = open(config_file, "w+")
     file.write(setup % (uuid, auth_token))
 
-def lookup_port():
+def lookup_port(name):
     # Looks for and returns port with description
     ports = serial.tools.list_ports.comports()
     for port in ports:
-        return port if 'NANOTDMRA' in port else None
+        return port if name in port else None
 
 
 def set_up_serial():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    serial_port_description = eval(config['SERIAL_SETTINGS']['SERIAL_PORT_DESCRIPTION'])
     # Waits 2 seconds before trying again if no port found
     # and doubles time each try with maximum 32 second waiting time
-    port = lookup_port()
+    port = lookup_port(serial_port_description)
     wait = 2
     while not port:
         print("No TCA found, waiting " + str(wait) + " seconds...")
         time.sleep(wait)
         if wait < 32:
             wait = wait*2
-        port = lookup_port()
+        port = lookup_port(serial_port_description)
 
     print("Serial port found: " + port.device)
 
     return serial.Serial(
         port = port.device,
-        baudrate = 115200,
-        parity = serial.PARITY_NONE,
-        stopbits = serial.STOPBITS_ONE,
-        bytesize = serial.EIGHTBITS,
-        timeout = 1
+        baudrate = eval(config['SERIAL_SETTINGS']['SERIAL_BAUDRATE']),
+        parity = eval(config['SERIAL_SETTINGS']['SERIAL_PARITY']),
+        stopbits = eval(config['SERIAL_SETTINGS']['SERIAL_STOPBITS']),
+        bytesize = eval(config['SERIAL_SETTINGS']['SERIAL_BYTESIZE']),
+        timeout = eval(config['SERIAL_SETTINGS']['SERIAL_TIMEOUT'])
     )
 
 def main():
@@ -80,8 +84,8 @@ def main():
                 location = raw_input("Enter location: ")
                 lat = raw_input("Enter latitude: ")
                 long = raw_input("Enter longitude: ")
-                username = raw_input("Enter username for CarbonMeasurmentApplication: ")
-                password = getpass.getpass('Enter password:')
+                username = raw_input("Enter username for '"+url+"':")
+                password = getpass.getpass("Enter password for '"+url+"':")
                 data = {'deviceId': uuid, 'location': location, 'lat': lat, 'long': long}
                 api = url + 'instruments/add'
                 response = requests.post(api, data=data, auth=(username,password))
