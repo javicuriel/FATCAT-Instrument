6f386f0742a404e1453fe16e1c54c817c6e6c5f7
javicuriel <javiercuriel94@gmail.com>
2018-06-07 14:53:37 +0200

    Logger test

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import ssl
+import logging
 
 client = mqtt.Client(
     client_id = 'd:{}:{}:{}'.format('brd98r', 'instrument', '11'),
@@ -30,6 +31,13 @@ def on_disconnect(client, userdata, flags, rc):
 client.on_connect = on_connect;
 client.on_disconnect = on_disconnect;
 client.tls_set(cert_reqs = ssl.CERT_REQUIRED, tls_version = ssl.PROTOCOL_TLSv1_2)
+
+# FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
+logging.basicConfig()
+logger = logging.getLogger('mqtt')
+logger.setLevel(logging.DEBUG);
+
+client.enable_logger(logger=logger)
 client.loop_start()
 
 while True:
