93782dbbfc7d3bd3044ca2be24d868f39eeb8ce4
javicuriel <javiercuriel94@gmail.com>
2018-06-07 14:35:13 +0200

    Dev2

--- test.py
+++ test.py
@@ -1,17 +1,24 @@
-try:
-    import httplib
-except:
-    import http.client as httplib
+import paho.mqtt.client as mqtt
 
-def have_internet():
-    conn = httplib.HTTPConnection("www.google.com", timeout=5)
-    try:
-        conn.request("HEAD", "/")
-        conn.close()
-        print("TRUE")
-        return True
-    except:
-        conn.close()
-        print("FALSE")
-        return False
-have_internet()
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, rc):
+    print("Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("$SYS/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("iot.eclipse.org", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
