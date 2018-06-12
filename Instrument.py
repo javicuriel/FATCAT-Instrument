f4f1bb6dd969a2db2166d691e49bf60892bef29e
javicuriel <javiercuriel94@gmail.com>
2018-06-12 10:57:37 +0200

    :bug:

--- Instrument.py
+++ Instrument.py
@@ -591,7 +591,7 @@ class Instrument(object):
                 self.log_message(module = "serial", msg = str(se), level = logging.WARN)
                 self._serial.close()
                 self._serial = self._set_up_serial()
-                self._serial.open()
+                # self._serial.open()
             except Exception as e:
                 self.log_message(module = "reading", msg = str(e), level = logging.WARN)
 
